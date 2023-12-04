4fe21d791025d6d23a61e97228f95645da51140d
BhargaVNaiduV <138513686+BhargaVNaiduV@users.noreply.github.com>
2023-12-03 22:40:18 -0500

    Update lambda_publisher_function.py
    We have added a function using Pydantic module to validate the responce payload we are getting from input

--- lambda_publisher_function.py
+++ lambda_publisher_function.py
@@ -8,11 +8,14 @@ try:
     import time
     from datetime import datetime
     from jinja2 import Environment, FileSystemLoader
+    from pydantic import BaseModel
 
     print("All modules are properly imported ...")
 
 except Exception as e:
     print(f"Error Occurred While Importing Python Modules: {e}")
+
+
 
 def employee_object_generater(EmployeeData):
     env = Environment(loader=FileSystemLoader('.'))
@@ -20,6 +23,32 @@ def employee_object_generater(EmployeeData):
     rendered_output = template.render(employee=EmployeeData)
     employee_dict = json.loads(rendered_output)
     return employee_dict
+def employee_data_validation(employee_data):
+    class LocalAddress(BaseModel):
+        apartment_number : str
+        street_name : str
+        city_name : str
+        zip_code : int
+
+    class EmployeeDataModel(BaseModel):
+        name : str
+        employee_id : int
+        local_address :  LocalAddress
+
+    try:
+        employee_data_validator = EmployeeDataModel(**employee_data)
+        print("Data provided is in valid format")
+        return dict(employee_data_validator)
+    except Exception as validation_error :
+        print("Error occured while validating the data")
+        for error in validation_error.errors():
+            if "employee_id" in error["loc"] and error["type"]=='int_parsing':
+                print("Invalid employee_id,Please provide valid employee_id.")
+                raise ValueError("Invalid employee_id,Please provide valid employee_id.")
+
+            if "zip_code" in error["loc"] and error["type"]=='int_parsing':
+                print("Invalid zip_code,Please provide valid zip_code.")
+                raise ValueError("Invalid zip_code,Please provide valid zip_code.")
 
 def lambda_handler(event, context):
     print(f"{datetime.now()} - Lambda function execution started.")
@@ -31,12 +60,12 @@ def lambda_handler(event, context):
     try:
         sqs = boto3.client('sqs')
         start_time = time.time()
-
+        employee_data=employee_data_validation(employee_data)
         message = employee_object_generater(employee_data)
         print(f"{datetime.now()} - The message we are sending right now is : {message}")
-        
+
         response = sqs.send_message(QueueUrl=sqs_queue_url, MessageBody=json.dumps(message))
-        
+
         print(f"{datetime.now()} - Message sent to the queue with url: {sqs_queue_url}")
         print(f"{datetime.now()} - Message sent to the queue. Response: {response}")
 
